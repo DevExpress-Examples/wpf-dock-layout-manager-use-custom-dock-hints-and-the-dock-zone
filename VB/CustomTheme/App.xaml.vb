d4f986e44725c7fdb5598b654cf4d24cdd075cb1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomTheme/App.xaml.vb
@@ -0,0 +1,16 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Windows
+
+Namespace CustomTheme
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Public Partial Class App
+		Inherits Application
+	End Class
+End Namespace

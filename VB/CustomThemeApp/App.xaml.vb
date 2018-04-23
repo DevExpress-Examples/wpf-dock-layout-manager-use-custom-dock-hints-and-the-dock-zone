840011c147981679d4c453534f0186b56d10ab8f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomThemeApp/App.xaml.vb
@@ -0,0 +1,8 @@
+Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace CustomThemeApp
+	Public Partial Class App
+		Inherits Application
+	End Class
+End Namespace
---
+++ VB/CustomThemeApp/Window1.xaml.vb
@@ -0,0 +1,11 @@
+Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace CustomThemeApp
+	Public Partial Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
